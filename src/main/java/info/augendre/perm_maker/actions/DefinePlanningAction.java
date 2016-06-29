90cd51ef1d5a2d3c373e19dc28383f56ed6ddaa6
Gabriel Augendre <gabriel@augendre.info>
2016-06-29 16:21:52 +0200

    Add planning to main panel

--- src/main/java/info/augendre/perm_maker/actions/DefinePlanningAction.java
+++ src/main/java/info/augendre/perm_maker/actions/DefinePlanningAction.java
@@ -22,6 +22,6 @@ public class DefinePlanningAction extends AbstractAction {
         dialog.pack();
         dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
-
+        mainPanel.refreshPlanningDisplay();
     }
 }
