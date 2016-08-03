db4084b6523e491d1d03274021b429b4da5d6e47
Gabriel Augendre <gabriel@augendre.info>
2016-08-03 17:49:07 +0200

    Add javadoc for Task. #13

--- src/main/java/info/augendre/perm_maker/data/Task.java
+++ src/main/java/info/augendre/perm_maker/data/Task.java
@@ -15,13 +15,47 @@ import java.util.*;
  */
 public class Task implements Serializable {
     private static final long serialVersionUID = -7058683052879161619L;
+
+    /**
+     * A map holding the resources assigned to the task for each day of week.
+     */
     private Map<DayOfWeek, ArrayList<Resource>> assignedResources;
+
+    /**
+     * The task's label (name).
+     */
     private String label;
+
+    /**
+     * The task's occurrences in the week.
+     */
     private Set<DayOfWeek> days;
+
+    /**
+     * The task's start time.
+     */
     private LocalTime startTime;
+
+    /**
+     * The task's end time.
+     */
     private LocalTime endTime;
+
+    /**
+     * The needed number of resources.
+     */
     private int numberOfResources;
 
+    /**
+     * Constructs a default task.
+     * <ul>
+     *     <li>Empty string for label</li>
+     *     <li>Empty set for days</li>
+     *     <li>Midnight as start and end time</li>
+     *     <li>No (0) resource needed</li>
+     *     <li>Empty map of assigned resources</li>
+     * </ul>
+     */
     public Task() {
         this.label = "";
         this.days = new HashSet<>();
@@ -31,6 +65,14 @@ public class Task implements Serializable {
         this.assignedResources = new HashMap<>();
     }
 
+    /**
+     * Constructs a task given the parameters.
+     * @param label The task's label.
+     * @param days The task's occurrences in the week.
+     * @param startTime The task's start time.
+     * @param endTime The task's end time.
+     * @param numberOfResources The number of resources needed for the task.
+     */
     public Task(String label, Set<DayOfWeek> days, LocalTime startTime, LocalTime endTime, int numberOfResources) {
         this.label = label;
         this.days = days;
@@ -47,6 +89,10 @@ public class Task implements Serializable {
                 endTime.get(ChronoField.HOUR_OF_DAY) + ":" + endTime.get(ChronoField.MINUTE_OF_HOUR) + ")";
     }
 
+    /**
+     * Returns a string description without the day of week in order to put in {@link info.augendre.perm_maker.gui.MainPanel}'s table.
+     * @return A string description without the day of week.
+     */
     public String toTableString() {
         return label + " (" + startTime.get(ChronoField.HOUR_OF_DAY) + ":" + startTime.get(ChronoField.MINUTE_OF_HOUR) + " - " +
                 endTime.get(ChronoField.HOUR_OF_DAY) + ":" + endTime.get(ChronoField.MINUTE_OF_HOUR) + ")";
@@ -108,6 +154,11 @@ public class Task implements Serializable {
         return assignedResources;
     }
 
+    /**
+     * Adds a resource to this task's assignments at the given day.
+     * @param resource The resource to add.
+     * @param day The day the resource is assigned.
+     */
     public void addAssignedResource(Resource resource, DayOfWeek day) {
         ArrayList<Resource> dayResources = this.assignedResources.get(day);
         if (dayResources == null) {
@@ -117,6 +168,10 @@ public class Task implements Serializable {
         dayResources.add(resource);
     }
 
+    /**
+     * Clears the assignments map.
+     * @see Map#clear() 
+     */
     public void clearAssignments() {
         this.assignedResources.clear();
     }
