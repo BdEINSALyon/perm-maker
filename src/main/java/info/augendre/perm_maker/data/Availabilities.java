1fce4488d65c418b9c777656e6b47172d65eefd8
Gabriel Augendre <gabriel@augendre.info>
2016-08-03 17:38:07 +0200

    Add constructor docs for Availabilities. #13

--- src/main/java/info/augendre/perm_maker/data/Availabilities.java
+++ src/main/java/info/augendre/perm_maker/data/Availabilities.java
@@ -12,13 +12,27 @@ import java.util.Collection;
 public class Availabilities extends ArrayList<Resource> implements Serializable {
     private static final long serialVersionUID = 4036489651363728736L;
 
+    /**
+     * Constructor with initial capacity.
+     * @param initialCapacity The initial capacity.
+     * @see ArrayList#ArrayList(int)
+     */
     public Availabilities(int initialCapacity) {
         super(initialCapacity);
     }
 
+    /**
+     * Default constructor.
+     * @see ArrayList#ArrayList()
+     */
     public Availabilities() {
     }
 
+    /**
+     * Constructor from existing collection.
+     * @param c Existing collection.
+     * @see ArrayList#ArrayList(Collection)
+     */
     public Availabilities(Collection<? extends Resource> c) {
         super(c);
     }
